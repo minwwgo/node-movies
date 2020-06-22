901bd75f19926e907652596e3745245cc6656aa1
ferhatu <ferhat_2001@yahoo.com>
2020-06-22 16:37:18 +0100

    add search movie by id

--- server.js
+++ server.js
@@ -1,25 +1,25 @@
 const express = require("express");
 const favoriteMovies = [
   {
-    id: 0,
+    id: 10,
     movieName: "Forest Gump",
     year: 1994,
     rating: 8.8,
   },
   {
-    id: 1,
+    id: 20,
     movieName: "Godfather",
     year: 1972,
     rating: 9.2,
   },
   {
-    id: 2,
+    id: 30,
     movieName: "Cast Away",
     year: 2001,
     rating: 7.8,
   },
   {
-    id: 3,
+    id: 40,
     movieName: "Schindler's List",
     year: 1994,
     rating: 8.9,
@@ -32,10 +32,15 @@ app.get("/", (req, res) => res.send("Hello Min's movies from API server"));
 app.get("/n", (req, res) => res.send("Hello Nader"));
 app.get("/f", (req, res) => res.send("Hello Ferhat"));
 
-function handleRequestForMovies(request, response) {
+function handleRequestForAllMovies(request, response) {
   response.send(favoriteMovies);
 }
-app.get("/movies", handleRequestForMovies);
+app.get("/movies", handleRequestForAllMovies);
+app.get("/movies/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const movie = favoriteMovies.find((movie) => movie.id === id);
+  movie ? res.send(movie) : res.status(404).send("No Movie found");
+});
 
 const myPort = 5555;
 app.listen(myPort, () => console.log(`Listening on port ${myPort}`));
