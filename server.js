d9135712b4f1b3d72324aaeaba2ddee81944e1a4
ferhatu <ferhat_2001@yahoo.com>
2020-06-22 15:42:01 +0100

    serving real movies data

--- server.js
+++ server.js
@@ -1,7 +1,30 @@
-
-
 const express = require("express");
-
+const favoriteMovies = [
+  {
+    id: 0,
+    movieName: "Forest Gump",
+    year: 1994,
+    rating: 8.8,
+  },
+  {
+    id: 1,
+    movieName: "Godfather",
+    year: 1972,
+    rating: 9.2,
+  },
+  {
+    id: 2,
+    movieName: "Cast Away",
+    year: 2001,
+    rating: 7.8,
+  },
+  {
+    id: 3,
+    movieName: "Schindler's List",
+    year: 1994,
+    rating: 8.9,
+  },
+];
 const app = express();
 
 app.get("/", (req, res) => res.send("Hello Min's movies from API server"));
@@ -9,12 +32,10 @@ app.get("/", (req, res) => res.send("Hello Min's movies from API server"));
 app.get("/n", (req, res) => res.send("Hello Nader"));
 app.get("/f", (req, res) => res.send("Hello Ferhat"));
 
-function handleRequestForMovies(request, response){
-  response.send("movie-one movie-two" )
+function handleRequestForMovies(request, response) {
+  response.send(favoriteMovies);
 }
-app.get('/movies', handleRequestForMovies)
-
-
+app.get("/movies", handleRequestForMovies);
 
 const myPort = 5555;
 app.listen(myPort, () => console.log(`Listening on port ${myPort}`));
