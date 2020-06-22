d5f720160fff920d6a9f1b934eabb54de5b4501b
mahmutsel <mahmutsel29@gmail.com>
2020-06-22 21:02:45 +0100

    fix myPort bug

--- server.js
+++ server.js
@@ -42,4 +42,4 @@ let port = process.env.PORT;
 if (port == null || port == "") {
   port = 5555;
 }
-app.listen(port, () => console.log(`Listening on port ${myPort}`));
+app.listen(port, () => console.log(`Listening on port ${port}`));
